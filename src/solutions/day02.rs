63df760cb7981699d3b5e604c0281485e6afb153
Antoine Cotten <hello@acotten.com>
2024-12-02 08:30:06 +0100

    Day 02, part 2

--- src/solutions/day02.rs
+++ src/solutions/day02.rs
@@ -1,4 +1,5 @@
 use crate::util::parse::ParseOps;
+use std::cmp::Ordering;
 use std::cmp::Ordering::{Equal, Greater, Less};
 
 pub fn parse(input: &str) -> Vec<Vec<u32>> {
@@ -19,11 +20,7 @@ pub fn part1(reports: &[Vec<u32>]) -> u32 {
             if first_lvl.abs_diff(**second_lvl) > 3 {
                 return false;
             }
-            let is_sorted = match first_lvl.cmp(second_lvl) {
-                Less => |a, b| a < b && b - a <= 3,
-                Greater => |a, b| b < a && a - b <= 3,
-                Equal => |_: &u32, _: &u32| false,
-            };
+            let is_sorted = is_sorted_fn(first_lvl.cmp(second_lvl));
             lvls_iter.is_sorted_by(|a: &&u32, b: &&u32| is_sorted(a, b))
         })
         .count()
@@ -31,8 +28,64 @@ pub fn part1(reports: &[Vec<u32>]) -> u32 {
         .unwrap()
 }
 
-pub fn part2(_: &[Vec<u32>]) -> u32 {
-    0
+pub fn part2(reports: &[Vec<u32>]) -> u32 {
+    reports
+        .iter()
+        .filter(|levels| {
+            let direction = direction(levels[..3].try_into().unwrap(), *levels.last().unwrap());
+            if direction == Equal {
+                return false;
+            };
+
+            let is_sorted = is_sorted_fn(direction);
+            if levels
+                .iter()
+                .is_sorted_by(|a: &&u32, b: &&u32| is_sorted(a, b))
+            {
+                return true;
+            }
+
+            let mut levels = (*levels).clone();
+            for i in 0..levels.len() {
+                let l = levels.remove(i);
+                if levels
+                    .iter()
+                    .is_sorted_by(|a: &&u32, b: &&u32| is_sorted(a, b))
+                {
+                    return true;
+                }
+                levels.insert(i, l);
+            }
+            false
+        })
+        .count()
+        .try_into()
+        .unwrap()
+}
+
+fn is_sorted_fn(ord: Ordering) -> fn(a: &u32, b: &u32) -> bool {
+    match ord {
+        Less => |a, b| a < b && b - a <= 3,
+        Greater => |a, b| b < a && a - b <= 3,
+        Equal => |_, _| false,
+    }
+}
+
+fn direction(beginning: [u32; 3], end: u32) -> Ordering {
+    // Possible scenarios:
+    // - "|3 2 1| ... 1" is Greater (decreasing)
+    // - "|1 2 3| ... 3" is Less (increasing)
+    // - "|2 3 4| ... 3" is Equal, which is a good indicator that the series has multiple direction
+    //   changes and therefore isn't "safe" in the first place.
+    beginning
+        .iter()
+        .map(|lvl| match lvl.cmp(&end) {
+            Less => -1,
+            Equal => 0,
+            Greater => 1,
+        })
+        .sum::<i32>()
+        .cmp(&0)
 }
 
 #[test]
@@ -47,6 +100,7 @@ fn sample_input() {
         ";
     let locs = parse(input);
     assert_eq!(part1(&locs), 2);
+    assert_eq!(part2(&locs), 4);
 }
 
 #[test]
@@ -56,6 +110,7 @@ fn first_two_levels_are_equal() {
         ";
     let locs = parse(input);
     assert_eq!(part1(&locs), 0);
+    assert_eq!(part2(&locs), 1);
 }
 
 #[test]
@@ -65,4 +120,38 @@ fn first_two_levels_are_far_apart() {
         ";
     let locs = parse(input);
     assert_eq!(part1(&locs), 0);
+    assert_eq!(part2(&locs), 1);
+}
+
+#[test]
+fn increases_direction_changes_after_first_level() {
+    let input = "\
+        9 3 6 7 8\n\
+        1 7 5 3 2\n\
+        ";
+    let locs = parse(input);
+    assert_eq!(part1(&locs), 0);
+    assert_eq!(part2(&locs), 2);
+}
+
+#[test]
+fn increase_direction_changes_after_second_level() {
+    let input = "\
+        6 8 4 3 1\n\
+        4 2 6 7 9\n\
+        ";
+    let locs = parse(input);
+    assert_eq!(part1(&locs), 0);
+    assert_eq!(part2(&locs), 2);
+}
+
+#[test]
+fn increase_direction_unclear() {
+    let input = "\
+        3 1 2 2\n\
+        1 2 3 2\n\
+        ";
+    let locs = parse(input);
+    assert_eq!(part1(&locs), 0);
+    assert_eq!(part2(&locs), 0);
 }
