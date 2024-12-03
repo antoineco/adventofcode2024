af5f0a476dbdb80858c86c2d3dfe7972cd00277b
Antoine Cotten <hello@acotten.com>
2024-12-03 13:15:27 +0100

    Day 03, part 2

--- src/solutions/day03.rs
+++ src/solutions/day03.rs
@@ -8,11 +8,32 @@ pub fn part1(muls: &[Mul]) -> u32 {
     muls.iter().map(|m| m.0 * m.1).sum()
 }
 
-pub fn part2(_: &[Mul]) -> u32 {
-    0
+pub fn part2(muls: &[Mul]) -> u32 {
+    let mut r#do = true;
+    muls.iter()
+        .map(|m| match m {
+            // Rogue values
+            Mul(0, b) => {
+                r#do = *b == 1;
+                0
+            }
+            _ => {
+                if r#do {
+                    m.0 * m.1
+                } else {
+                    0
+                }
+            }
+        })
+        .sum()
 }
 
 pub struct Mul(u32, u32);
+
+// Rogue values.
+// 0 product to avoid skewing results.
+const DO: Mul = Mul(0, 1);
+const DONT: Mul = Mul(0, 0);
 
 impl From<Vec<u32>> for Mul {
     fn from(value: Vec<u32>) -> Self {
@@ -48,8 +69,17 @@ fn try_mul(bytes: &mut Bytes) -> Option<Mul> {
     let mut last_three = [0_u8; 3];
     loop {
         let byte = bytes.next()?;
+        // reached "do()"
+        if byte == b')' && last_three == [b'd', b'o', b'('] {
+            return Some(DO);
+        }
+        // reached "'t()" - for "don't()"
+        // Hacky but works on the puzzle's input.
+        else if byte == b')' && last_three == [b'\'', b't', b'('] {
+            return Some(DONT);
+        }
         // reached "mul("
-        if byte == b'(' && last_three.iter().eq([b'm', b'u', b'l'].iter()) {
+        else if byte == b'(' && last_three == [b'm', b'u', b'l'] {
             (last_three[0], last_three[1], last_three[2]) = (last_three[1], last_three[2], byte);
             let mut numbers: Vec<u32> = Vec::with_capacity(2);
             let mut digits = String::with_capacity(3);
@@ -96,10 +126,11 @@ fn try_mul(bytes: &mut Bytes) -> Option<Mul> {
 #[test]
 fn sample_input() {
     let input = "\
-        xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))\n\
+        xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))\n\
         ";
     let res = parse(input);
     assert_eq!(part1(&res), 161);
+    assert_eq!(part2(&res), 48);
 }
 
 #[test]
@@ -110,4 +141,5 @@ fn mul_in_mul() {
         ";
     let res = parse(input);
     assert_eq!(part1(&res), 80);
+    assert_eq!(part2(&res), 80);
 }
