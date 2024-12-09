6eee4c958b4c82f77ad4a16dc1173c961ffcf14d
Antoine Cotten <hello@acotten.com>
2024-12-09 18:26:58 +0100

    Day 09, part 2

--- src/solutions/day09.rs
+++ src/solutions/day09.rs
@@ -78,8 +78,76 @@ pub fn part1(diskmap: &[Block]) -> usize {
         .0
 }
 
-pub fn part2(_: &[Block]) -> usize {
-    0
+pub fn part2(diskmap: &[Block]) -> usize {
+    let mut compacted = diskmap.to_vec();
+
+    // A possible optimization here would be to keep track of all free blocks indexed by length,
+    // and to check them off as we swap them with files. This would help reducing the number of
+    // iterations even further, based on the length of the file being evaluated.
+    let mut first_free = 1;
+
+    let mut j = compacted.len() - 1;
+    while j > first_free {
+        let Block::File { len: file_len, .. } = compacted[j] else {
+            if j == compacted.len() - 1 {
+                compacted.truncate(compacted.len() - 1);
+            }
+            j -= 1;
+            continue;
+        };
+
+        let mut first_free_seen = None;
+        let mut moved = false;
+        let mut i = first_free;
+        while i < j {
+            let Block::Free(free_len) = compacted[i] else {
+                i += 1;
+                continue;
+            };
+            if first_free_seen.is_none() {
+                first_free_seen = Some(i);
+            }
+
+            match file_len.cmp(&free_len) {
+                Ordering::Equal => {
+                    compacted.swap(i, j);
+                    moved = true;
+                    break;
+                }
+                Ordering::Less => {
+                    compacted.insert(i + 1, Block::Free(free_len - file_len));
+                    compacted[i] = Block::Free(file_len);
+                    j += 1;
+                    compacted.swap(i, j);
+                    moved = true;
+                    break;
+                }
+                Ordering::Greater => i += 1,
+            }
+        }
+
+        let Some(first_free_seen) = first_free_seen else {
+            break;
+        };
+        if first_free_seen > first_free {
+            first_free = first_free_seen;
+        }
+
+        if !moved {
+            j -= 1;
+        }
+    }
+
+    compacted
+        .iter()
+        .fold((0, 0), |(acc, offset), f| match f {
+            Block::File { id, len } => (
+                acc + (offset..(offset + len)).fold(0, |acc, mul| acc + (id * mul)),
+                offset + len,
+            ),
+            Block::Free(len) => (acc, offset + len),
+        })
+        .0
 }
 
 #[derive(Clone)]
@@ -95,6 +163,7 @@ fn sample_input() {
         ";
     let diskmap = parse(input);
     assert_eq!(part1(&diskmap), 1928);
+    assert_eq!(part2(&diskmap), 2858);
 }
 
 #[test]
@@ -104,4 +173,5 @@ fn trivial() {
         ";
     let diskmap = parse(input);
     assert_eq!(part1(&diskmap), 60);
+    assert_eq!(part2(&diskmap), 132);
 }
